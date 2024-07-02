ab0e74103debb80c87cd60e4ffa85d3d2ef93bf9
Phann020126 <daniel020126@gmail.com>
2024-07-02 19:25:00 -0400

    add row and col to token

--- src/hulk/hulk_lexer.py
+++ src/hulk/hulk_lexer.py
@@ -1,12 +1,12 @@
-from cmp.utils import Token
+from utils import Token
+from lexer.regex import Regex
 from cmp.automata import State
-from hulk.lexer.regex import Regex
 
 
 class Lexer:
     def __init__(self, table, eof):
+        self.errors = []
         self.eof = eof
-        self.cont = 0
         self.regexs = self._build_regexs(table)
         self.automaton = self._build_automaton()
 
@@ -19,17 +19,17 @@ class Lexer:
                     v.tag = (n, token_type)
             regexs.append(states)
         return regexs
-
+    
     def _build_automaton(self):
-        start = State("start")
+        start = State('start')
         for regex in self.regexs:
             start.add_epsilon_transition(regex)
         return start.to_deterministic()
-
+    
     def _walk(self, string):
         state = self.automaton
         final = state if state.final else None
-        final_lex = lex = ""
+        final_lex = lex = ''
         for symbol in string:
             new_state = state[symbol]
             if new_state is not None and new_state[0] is not None:
@@ -45,17 +45,41 @@ class Lexer:
                 break
 
         return final, final_lex
-
+    
     def _tokenize(self, text):
+        
+        row = 1
+        column = 1
+
         while text:
+            
+            if text[0] == ' ':
+                column += 1
+                text = text[1:]
+                continue
+
+            if text[0] == '\n':
+                row += 1
+                column = 1
+                text = text[1:]
+                continue
+            
             final, final_lex = self._walk(text)
+            
             if len(final_lex) == 0:
-                # Error
+                self.errors.append(f"Invalid token at line: {row}, column: {column}")
                 break
+
             n, token_type = min(final.tag)
-            text = text[len(final_lex) :]
-            yield final_lex, token_type
-        yield "$", self.eof
+
+            text = text[len(final_lex):]
+
+            yield final_lex, token_type, row, column
+
+            row += len(final_lex)
+
+        yield '$', self.eof
 
     def __call__(self, text):
-        return [Token(lex, token_type) for lex, token_type in self._tokenize(text)]
+        return [Token(lex, token_type, row, column) for lex, token_type, row, column in self._tokenize(text)]
+
--- src/hulk/utils.py
+++ src/hulk/utils.py
@@ -1,1 +1,9 @@
+from cmp.utils import Token as BaseToken
+
 EPSILON = 'ε'
+
+class Token(BaseToken):
+    def __init__(self, lex, token_type, row, column):
+        super().__init__(lex, token_type)
+        self.row = row
+        self.column = column
