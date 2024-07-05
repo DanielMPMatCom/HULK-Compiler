1c9c3426c9b907dd5e111b2ad5d3f3f84cc5f419
Phann020126 <daniel020126@gmail.com>
2024-07-04 20:03:34 -0400

    toledo se fue a bnnar

--- src/cmp/semantic.py
+++ src/cmp/semantic.py
@@ -2,6 +2,7 @@ import itertools as itt
 from collections import OrderedDict
 from hulk.hulk_ast import*
 from typing import List
+from hulk.semantic_check.semantic_utils import TypeDTONode
 
 #region Errors
 class SemanticError(Exception):
@@ -293,6 +294,7 @@ class Context:
         self.types = {}
         self.functions = {}
         self.protocols = {}
+        self.data_type_nodes = {}
 
     def create_type(self, name:str):
         if name in self.types:
@@ -348,6 +350,9 @@ class Context:
                 return self.get_type(type_)
             except SemanticError:
                 return self.get_protocol(type_)
+    
+    def save_type_info(self, type_name : str, type_dto_node: TypeDTONode):
+        self.data_type_node[type_name] = type_dto_node
 
 
     def __str__(self):
---
+++ src/hulk/semantic_check/semantic_utils.py
@@ -0,0 +1,7 @@
+from src.hulk.hulk_grammar import *
+
+class TypeDTONode():
+    param_names : list[str] = []
+    attributes : list[AttributeNode] = []
+    methods : list[MethodNode] = []
+
