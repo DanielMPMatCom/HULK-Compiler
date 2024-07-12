ed94c0d7bf02184ec6b9be3c24b20447584d58e7
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2024-07-12 15:52:20 -0400

    tubirririri

--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ from cmp.evaluation import evaluate_reverse_parse_plus
 from cmp.formatVisitor import FormatVisitor
 from hulk.semantic_check.semantic_check_pipeline import semantic_check_pipeline
 from hulk.tree_interpeter.interpeter import Interpreter
+import sys
+
+sys.setrecursionlimit(1000000)
 
 load = True
 save = not load
@@ -18,96 +21,192 @@ lexer = Lexer(table=table, eof=G.EOF, load=load, save=save)
 
 
 code_example = r"""
-type Point(x, y) {
+function tan(x: Number): Number => sin(x) / cos(x);
+function cot(x) => 1 / tan(x);
+function operate(x, y) {
+    print(x + y);
+    print(x - y);
+    print(x * y);
+    print(x / y);
+}
+function fib(n) => if (n == 0 | n == 1) 1 else fib(n-1) + fib(n-2);
+function fact(x) => let f = 1 in for (i in range(1, x+1)) f := f * i;
+function gcd(a, b) => while (a > 0)
+        let m = a % b in {
+            b := a;
+            a := m;
+        };
+protocol Hashable {
+    hash(): Number;
+}
+protocol Equatable extends Hashable {
+    equals(other: Object): Boolean;
+}
+type Point(x,y) {
     x = x;
     y = y;
+
     getX() => self.x;
     getY() => self.y;
 
     setX(x) => self.x := x;
     setY(y) => self.y := y;
+    hash() : Number {
+        5;
+    }
 }
-
-type PolarPoint inherits Point {
+type PolarPoint(phi, rho) inherits Point(rho * sin(phi), rho * cos(phi)) {
     rho() => sqrt(self.getX() ^ 2 + self.getY() ^ 2);
 }
-
+type Knight inherits Person {
+    name() => "Sir" @@ base();
+}
 type Person(firstname, lastname) {
     firstname = firstname;
     lastname = lastname;
 
     name() => self.firstname @@ self.lastname;
+    hash() : Number {
+        5;
+    }
 }
-
-type Knight inherits Person {
-    name() => "Sir" @@ base();
-}
-
-type Bird {
-    name() => "Bird";
-}
-
-type Plane {
-    name() => "Plane";
-}
-
 type Superman {
-    name() => "Superman";
 }
-
-
+type Bird {
+}
+type Plane {
+}
 type A {
-    prop() => "A";
+    hello() => print("A");
 }
 
 type B inherits A {
-    prop() => "B";
+    hello() => print("B");
 }
 
 type C inherits A {
-    prop() => "C";
+    hello() => print("C");
 }
-
-function gcd(a, b) {
-    if (b == 0)  a
-    else gcd(b, a % b);
-}
-
-function fib(n) {
-    if (n <= 1) 1
-    else {fib(n - 1) + fib(n - 2)};
-}
-
-type Recursive {
-    r(n) => if (n == 0 | n == 1) 1 else self.r(n-1) + self.r(n-2); 
-}
-
-function fact(x) => let f = 1 in for (i in range(1, x+1)) f := f * i;
-
-
 
 {
+    42;
+    print(42);
+    print((((1 + 2) ^ 3) * 4) / 5);
+    print("Hello World");
+    print("The message is \"Hello World\"");
+    print("The meaning of life is " @ 42);
+    print(sin(2 * PI) ^ 2 + cos(3 * PI / log(4, 64)));
+    {
+        print(42);
+        print(sin(PI/2));
+        print("Hello World");
+    }
+
+
+    print(tan(PI) ** 2 + cot(PI) ** 2);
+
+    let msg = "Hello World" in print(msg);
+    let number = 42, text = "The meaning of life is" in
+        print(text @ number);
+    let number = 42 in
+        let text = "The meaning of life is" in
+            print(text @ number);
+    let number = 42 in (
+        let text = "The meaning of life is" in (
+                print(text @ number)
+            )
+        );
+    let a = 6, b = a * 7 in print(b);
+    let a = 6 in
+        let b = a * 7 in
+            print(b);
+    let a = 5, b = 10, c = 20 in {
+        print(a+b);
+        print(b*c);
+        print(c/a);
+    };
+    let a = (let b = 6 in b * 7) in print(a);
+    print(let b = 6 in b * 7);
+    let a = 20 in {
+        let a = 42 in print(a);
+        print(a);
+    };
+    let a = 7, a = 7 * 6 in print(a);
+    let a = 7 in
+        let a = 7 * 6 in
+            print(a);
+    let a = 0 in {
+        print(a);
+        a := 1;
+        print(a);
+    };
+    let a = 0 in
+        let b = a := 1 in {
+            print(a);
+            print(b);
+        };
+    let a = 42 in if (a % 2 == 0) print("Even") else print("odd");
+    let a = 42 in print(if (a % 2 == 0) "even" else "odd");
+    let a = 42 in
+        if (a % 2 == 0) {
+            print(a);
+            print("Even");
+        }
+        else print("Odd");
+    let a = 42, mod = a % 3 in 
+        print(
+            if (mod == 0) "Magic"
+            elif (mod % 3 == 1) "Woke"
+            else "Dumb"
+        );
+    let a = 10 in while (a >= 0) {
+        print(a);
+        a := a - 1;
+    };
     
+    for (x in range(0, 10)) print(x);
+    let iterable = range(0, 10) in
+        while (iterable.next())
+            let x = iterable.current() in
+                print(x);
+
+    let pt = new Point(0, 0) in
+        print("x: " @ pt.getX() @ "; y: " @ pt.getY());
+    let pt = new Point(3,4) in
+        print("x: " @ pt.getX() @ "; y: " @ pt.getY());
+    let pt = new PolarPoint(3,4) in
+        print("rho: " @ pt.rho());
+
+    let p = new Knight("Phil", "Collins") in
+        print(p.name());
+    let p: Person = new Knight("Phil", "Collins") in print(p.name());
+    let x: Number = 42 in print(x);
+
+    let x = new Superman() in
+        print(
+            if (x is Bird) "It's bird!"
+            elif (x is Plane) "It's a plane!"
+            else "No, it's Superman!"
+        );
+    let x = 42 in print(x);
+    let total = ({ print("Total"); 5; }) + 6 in print(total);
+    let x : A = if (rand() < 0.5) new B() else new C() in
+        if (x is B)
+            let y : B = x as B in {
+                y.hello();
+            }
+        else {
+            print("x cannot be downcasted to B");
+        };
+
     let numbers = [1,2,3,4,5,6,7,8,9] in
         for (x in numbers)
-            if (x % 2 == 0)
-                print("even" @@ x)
-            else
-                print("odd" @@ x);
-    
+            print(x);
     let numbers = [1,2,3,4,5,6,7,8,9] in print(numbers[7]);
-
-    let squares = [x^2 || x in range(1,10)] in print(squares);
-    let test = [x%2==0 || x in [1,2,3,4,5,6,7,8,9]] in print("test" @@ (if (test[0]) test[0] else "es False"));
-
-            
-    let x = new Recursive() in
-    print(x.r(5));
-    print(fact(5));
-
-    let x = while(10 < 5) print("esto no se imprime") in print("esto si se imprime" @@ x);
-
-    let b = new B() in print(b);
+    let squares = [x^2 || x in range(1,10)] in print(squares); 
+    let squares = [x^2 || x in range(1,10)] in for (x in squares) print(x);
+    let x : Hashable = new Person("juan", "pablo") in print(x.hash());
+    let x : Hashable = new Point(0,0) in print(x.hash()); 
 }
 """
 print(code_example)
